9b7845fd8e3912f82da176dbc2acb67ee8c0ee43
Alexey Voblikov <1nro@bk.ru>
2022-08-31 15:17:16 +0400

    внесли изменения в example001

--- example001/Program.cs
+++ example001/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World! 222");
+Console.WriteLine("Hello, World! 222");
+Console.WriteLine("Hello, World! 222");
